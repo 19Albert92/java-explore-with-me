b8b68be2a8b31d9b4891c67e988dfcd28d87b721
Albertinio <albertinio@yandex.ru>
2026-05-31 23:58:30 +0300

    [fix] исправление по рекомендациям ревьюера. Добавил интеграционный тест для слоя контроллера.

--- stat-service/stat-dto/src/main/java/shared/UtilConstant.java
+++ stat-service/stat-dto/src/main/java/shared/UtilConstant.java
@@ -7,6 +7,5 @@ import java.time.format.DateTimeFormatter;
 public class UtilConstant {
 
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
 }
